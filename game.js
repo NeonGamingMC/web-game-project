1d77af7d73f0e27c9a185446bb7e9eae64d9c253
NeonGaming <85440795+NeonGamingMC@users.noreply.github.com>
2021-10-04 11:04:58 +0900

    update 1

--- game.js
+++ game.js
@@ -17,9 +17,9 @@ function canvasClick() {
 }
 function update() {
     ctx.fillStyle = "rgba(255,255,255,1)";
-    ctx.fillRect(0,0,1500,1000)
+    ctx.fillRect(0,0,gameCan.width,gameCan.height)
     ctx.strokeStyle = "rgba(0,0,0,1)";
-    ctx.strokeRect(2,2,1496,796);
+    ctx.strokeRect(2,2,gameCan.width-4,gameCan.height-4);
     if (started){
         birb.y += birb.fallSpd;
         birb.fallSpd += 1;
@@ -28,10 +28,11 @@ function update() {
         }
         ctx.fillStyle = "rgba(255,200,0,1)";
         ctx.beginPath();
-        ctx.arc(400,birb.y,20,0,Math.PI*2);
+        ctx.arc(gameCan.width/3,birb.y,20,0,Math.PI*2);
         ctx.fill();
     }else{
         ctx.fillStyle = "rgba(0,0,0,0.25)"
-        ctx.fillRect(200,200,1100,200)
+        ctx.fillRect(200,200,gameCan.width-400,200)
+        ctx.fillStyle = "rgba(255,200,0,1"
     }
 }
