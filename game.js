2c6e7be1d80f1170ebeb44b6dd54f65024a77439
NeonGaming <85440795+NeonGamingMC@users.noreply.github.com>
2021-10-06 09:54:48 +0900

    update 2

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ var ctx = gameCan.getContext("2d");
 var started = false;
 var birb = {
     y: 400,
-    fallSpd: -10
+    fallSpd: -10,
+    alive: true
 }
 gameCan.addEventListener("click",canvasClick);
 setInterval(update,20);
@@ -18,21 +19,27 @@ function canvasClick() {
 function update() {
     ctx.fillStyle = "rgba(255,255,255,1)";
     ctx.fillRect(0,0,gameCan.width,gameCan.height)
-    ctx.strokeStyle = "rgba(0,0,0,1)";
-    ctx.strokeRect(2,2,gameCan.width-4,gameCan.height-4);
+    ctx.fillStyle = "rgba(255,200,0,1)";
+    ctx.beginPath();
+    ctx.arc(gameCan.width/3,birb.y,20,0,Math.PI*2);
+    ctx.fill();
     if (started){
         birb.y += birb.fallSpd;
         birb.fallSpd += 1;
         if (birb.fallSpd > 20){
             birb.fallSpd = 20
         }
-        ctx.fillStyle = "rgba(255,200,0,1)";
-        ctx.beginPath();
-        ctx.arc(gameCan.width/3,birb.y,20,0,Math.PI*2);
-        ctx.fill();
+        if (birb.y<60) or (birb.y>gameCan.width-60);{
+            birb.alive = false;
+        }
     }else{
         ctx.fillStyle = "rgba(0,0,0,0.25)"
         ctx.fillRect(200,200,gameCan.width-400,200)
-        ctx.fillStyle = "rgba(255,200,0,1"
+        ctx.fillStyle = "rgba(255,200,0,1)"
+        ctx.font = "60px sans-serif"
+        ctx.textAlign = "center"
+        ctx.fillText("flying birb",gameCan.width/2,300)
     }
+    ctx.strokeStyle = "rgba(0,0,0,1)";
+    ctx.strokeRect(2,2,gameCan.width-4,gameCan.height-4);
 }
