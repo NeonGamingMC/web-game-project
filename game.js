839e2877baa4428e33174c6985c0dd9fd1b2b11d
NeonGaming <85440795+NeonGamingMC@users.noreply.github.com>
2021-11-30 08:26:18 +0900

    more death

--- game.js
+++ game.js
@@ -10,6 +10,11 @@ playerImg.onload = function () {
     imagesLoaded += 1
 }
 playerImg.src = 'assets/player.png';
+var heartImg = new Image();
+heartImg.onload = function () {
+    imagesLoaded += 1
+}
+heartImg.src = 'assets/heart.png';
 var gameCan = document.getElementById("gameCanvas")
 var ctx = gameCan.getContext("2d")
 var started = false;
@@ -18,7 +23,8 @@ var playerImg;
 var player = {
     y: 400,
     fallSpd: 0,
-    alive: true
+    alive: true,
+    lives: 3
 }
 class obstacle {
     constructor(x, y, size) {
@@ -42,10 +48,11 @@ class particle{
 }
 var obstacles = new Array();
 var particles = new Array();
-for (let i = 0;i<333;i++){
+for (let i = 0;i<500;i++){
     particles.push(new particle(Math.random()*gameCan.width, Math.random()*gameCan.height,-1+Math.random()*-4,0,0,0,"rgba(170,170,170,1)",5,5));
 }
 var obstacleCount = 0;
+var gameOver = false;
 obstacles.push(new obstacle(0,Math.random()*gameCan.width,50));
 gameCan.addEventListener("mousedown",down);
 gameCan.addEventListener("mouseup",up);
@@ -54,15 +61,15 @@ setInterval(spawnObstacle,1000);
 function down() {
     if(!started){
         started = true;
-    }else if (!player.alive){
+    }else if (!player.alive&&!gameOver){
         player.alive = true;
         player.y = 400;
         player.fallSpd = 0;
+        player.lives -= 1;
         obstacleCount = 0;
-        obstacles.forEach(function (item,index,array){
-            array.pop()
-        })
-        obstacles.push(new obstacle(0-(50+200*(obstacleCount/200)*2)/2,Math.random()*gameCan.width,50+200*(obstacleCount/200)));
+        for (var i = 0;obstacles.length>0;i++){
+            obstacles.shift()
+        }
     }
     mouseDown = true;
 }
@@ -70,37 +77,38 @@ function up(){
     mouseDown = false;
 }
 function update() {
-    if (imagesLoaded==2){
-        particles.push(new particle(gameCan.width, Math.random()*gameCan.height,-1+Math.random()*-4,0,0,0,"rgba(170,170,170,1)",5,5));
+    if (imagesLoaded==3){
+        if (player.alive){
+            particles.push(new particle(gameCan.width, Math.random()*gameCan.height,-1+Math.random()*-4,0,0,0,"rgba(170,170,170,1)",5,5));
+        }
         ctx.fillStyle = "rgba(0,0,0,1)";
         ctx.fillRect(0,0,gameCan.width,gameCan.height)
-        particles.forEach(function (item,index,array){
-            ctx.fillStyle = item.color;
-            ctx.fillRect(item.x,item.y,item.sizex,item.sizey);
-            item.x += item.dx;
-            item.y += item.dy;
-            item.dx += item.fx;
-            item.dy += item.fy;
-            if (item.x>gameCan.width){
-                array.shift();
+        for (var i = 0;i<particles.length;i++){
+            ctx.fillStyle = particles[i].color;
+            ctx.fillRect(particles[i].x,particles[i].y,particles[i].sizex,particles[i].sizey);
+            if (player.alive){
+                particles[i].x += particles[i].dx;
+                particles[i].y += particles[i].dy;
+                particles[i].dx += particles[i].fx;
+                particles[i].dy += particles[i].fy;
+                if (particles[i].x>gameCan.width){
+                    particles.shift();
+                }
             }
-        })
+        }
         if (player.alive){
             ctx.drawImage(playerImg,gameCan.width/3,player.y,40,40);
         }
-        obstacles.forEach(function (item,index,array){
-            ctx.drawImage(obstacleImg,gameCan.width-item.x-item.size/2,item.y-item.size/2,item.size,item.size);
-            if (item.x>gameCan.width){
-                array.shift()
+        for (var i = 0;i<obstacles.length;i++){
+            ctx.drawImage(obstacleImg,gameCan.width-obstacles[i].x-obstacles[i].size/2,obstacles[i].y-obstacles[i].size/2,obstacles[i].size,obstacles[i].size);
+            if (obstacles[i].x>gameCan.width){
+                obstacles.shift()
             }
-            //if(item.x > gameCan.width/3 && x < gameCan.width/3+40 && item.y > player.y && item.y < player.y+40) {
-            //    player.alive = false;
-            //    obstacleCount = 0;
-            //    array.forEach(function (item,index,array){
-            //        array.shift()
-            //    })
-            //}
-        })
+            if(gameCan.width-obstacles[i].x+obstacles[i].size > gameCan.width/3 && gameCan.width-obstacles[i].x < gameCan.width/3+40 && obstacles[i].y > player.y && obstacles[i].y < player.y+40) {
+                player.alive = false;
+                onPlayerDeath()
+            }
+        }
         if (started && player.alive){
             player.y += player.fallSpd;
             player.fallSpd += 0.3;
@@ -109,27 +117,36 @@ function update() {
             }
             if (player.y<0|player.y>gameCan.height){
                 player.alive = false;
+                onPlayerDeath()
             }
             for (let i = 0;i<obstacles.length;i++){
                 obstacles[i].x += 5
             }
-        }else if (!started&&player.alive){
+        }else if (!started&&player.alive&&!gameOver){
             ctx.fillStyle = "rgba(255,255,255,0.25)"
             ctx.fillRect(200,200,gameCan.width-400,200)
             ctx.fillStyle = "rgba(0,255,255,1)"
             ctx.font = "60px sans-serif"
             ctx.textAlign = "center"
             ctx.fillText("game title here",gameCan.width/2,300)
-        }else if (started&&!player.alive){
-            ctx.fillStyle = "rgba(255,0,0,1)"
-            ctx.font = "60px sans-serif"
+        }else if (started&&!player.alive&&!gameOver){
+            ctx.fillStyle = "rgba(255,255,255,1)"
+            ctx.font = "40px sans-serif"
             ctx.textAlign = "center"
-            ctx.fillText("tap try again",gameCan.width/2,gameCan.height/2)
+            ctx.fillText("tap to try again",gameCan.width/2,gameCan.height/2)
+        }else if (started&&!player.alive&&gameOver){
+            ctx.fillStyle = "rgba(255,0,0,1)"
+            ctx.font = "64px sans-serif"
+            ctx.textAlign = "center"
+            ctx.fillText("game over",gameCan.width/2,gameCan.height/2)
+        }
+        for (var i = 0; i<player.lives; i++){
+            ctx.drawImage(heartImg,76*i+10,10);
         }
         ctx.strokeStyle = "rgba(255,255,255,0.5)";
         ctx.strokeRect(2,2,gameCan.width-4,gameCan.height-4);
         if (started&mouseDown){
-            player.fallSpd -= 0.6
+            player.fallSpd -= 0.6;
         }
     }
 }
@@ -137,5 +154,10 @@ function spawnObstacle(){
     if (started&&player.alive){
         obstacles.push(new obstacle(0-(50+200*(obstacleCount/200)*2)/2,Math.random()*gameCan.width,50+200*(obstacleCount/200)));
         obstacleCount += 1;
+    }
+}
+function onPlayerDeath(){
+    if (player.lives<1){
+        gameOver = true;
     }
 }
